8f52c44cc6c0d663c5ceef2eea92aea759d20899
Petr Stodůlka <xstodu05@gmail.com>
2020-05-06 17:05:18 +0200

    Mod/acme storage (#1)
    * Add example actors for ACME Storage
    
    * Update acme/actors/acmestoragemigrator/actor.py
    
    Co-authored-by: Petr Stodůlka <xstodu05@gmail.com>
    
    * Update acme/actors/acmestoragescanner/actor.py
    
    Co-authored-by: Petr Stodůlka <xstodu05@gmail.com>
    
    * Split acme storagescanner to two actors
    
    The whole solution for acme contains now 3 actors, kind of
     - scanner
     - checker
     - migrator
    
    As well, raise an error in case we cannot obtain list of active
    kernel modules.
    
    * Create new hierarhy for ISV-repos and link acme to the official one
    
    Previously it was expected that users will link the repository using
    snactor. But possibly many solutions could be created just for purpose
    of inplace upgrade between specific systems (e.g. RHEL 7 -> 8) and
    will not be valid for upgrade (RHEL 8 -> 9).
    
    To make it clear, let's use the el7toel8 directory.
    
    As the ACME repository clearly is now just for inplace upgrade from
    RHEL 7 to RHEL 8, link it directly to the official
    system_upgrade_el7toel8 repository.
    
    * Update directory hierarchy in the README.md file
    
    * ACME: run acmemigrator in the last phase and fix nitpicks
    
    The ACME probably will not be loaded inside the initrd so nothing
    can be migrated there. Instead of that, migrate the ACME storage
    during the first boot.
    
    Co-authored-by: Pavel Odvody <podvody@redhat.com>

---
+++ el7toel8/acme/actors/acmestoragescanner/actor.py
@@ -0,0 +1,42 @@
+import os
+
+from leapp.actors import Actor
+from leapp.exceptions import StopActorExecutionError
+from leapp.libraries.common.rpms import has_package
+from leapp.models import AcmeStorageInfo, ActiveKernelModulesFacts, InstalledRPM
+from leapp.tags import FactsPhaseTag, IPUWorkflowTag
+
+
+class AcmeStorageScanner(Actor):
+    """
+    Scan the system for ACME Storage
+    """
+
+    name = 'acme_storage_scanner'
+    consumes = (ActiveKernelModulesFacts, InstalledRPM)
+    produces = (AcmeStorageInfo,)
+    tags = (FactsPhaseTag, IPUWorkflowTag)
+
+    def process(self):
+        # Check if our package is installed
+        if not has_package(InstalledRPM, 'acme-storage'):
+            return
+
+        # Get a list of active kernel modules
+        kernel_module_facts = next(self.consume(ActiveKernelModulesFacts), None)
+        if not kernel_module_facts:
+            # hypothetic error; it should not happen
+            raise StopActorExecutionError('Unable to obtain list of active kernel modules.')
+            return
+
+        # Detect if our kernel module is installed
+        has_kernel_module = False
+        for active_module in kernel_module_facts.kernel_modules:
+            if active_module.filename == 'acme8xx':
+                has_kernel_module = True
+                break
+
+        # Is the device installed in the default location?
+        has_device = os.path.exists('/dev/acme0')
+
+        self.produce(AcmeStorageInfo(has_kernel_module=has_kernel_module, has_device=has_device))
